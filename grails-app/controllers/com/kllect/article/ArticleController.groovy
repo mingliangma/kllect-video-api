37e7582e13654ce0b11989e7fb3dad7a409ca1b5
Mingliang Ma <mingliang.ma@gmail.com>
2016-04-02 23:34:49 -0400

    clean up

--- grails-app/controllers/com/kllect/article/ArticleController.groovy
+++ grails-app/controllers/com/kllect/article/ArticleController.groovy
@@ -8,10 +8,7 @@ import org.bson.types.ObjectId
 
 class ArticleController {
     def listVideoArticles() {
-        def articles = Article.findAllByIs_video(true)
-//        String idString = "56ec73f451ac840baf4fea4e"
-//        def articles = Article.get(new ObjectId("56ec73f451ac840baf4fea4e"))
-//        respond articles
+        def articles = Article.findAllByIs_video(true, [max: 30, sort: "parse_date", order: "desc"])
         [article: articles]
     }
 
