4808c2d752d6d41b89dd0b5011e941435f558604
Mingliang Ma <mingliang.ma@gmail.com>
2016-12-16 16:53:23 -0500

    - added topic ID validation

--- grails-app/controllers/com/kllect/user/InterestController.groovy
+++ grails-app/controllers/com/kllect/user/InterestController.groovy
@@ -14,6 +14,12 @@ class InterestController {
         String token = request.JSON.token
         def topicIds = request.JSON.topicIds
 
+        log.info("Token: "+token)
+        log.info("topicIds: "+topicIds)
+
+        println "Token: "+token
+        println "topicIds: "+topicIds
+
         JWTPayload jwtPayload;
         try {
             jwtPayload = verifyService.verifyToken(token)
@@ -30,9 +36,14 @@ class InterestController {
         }
 
         User u = saveNewUser(jwtPayload)
-
-        saveInterests(topicIds, u)
-
+        try {
+            saveInterests(topicIds, u)
+        }catch (InvalidUserInputException e){
+                log.error("InvalidUserInputException: "+e.message)
+                response.status= 400
+                render(view:'authFailed', model:[error: e.getMessage()])
+                return
+            }
         if (!u.save(flush: true)) {
             u.errors.allErrors.each {
                 log.error(it)
@@ -59,17 +70,19 @@ class InterestController {
         for (String topic : interests) {
 
             if (ObjectId.isValid(topic)) {
-                println "topic: " + topic
+                log.info( "topic: " + topic)
                 Topic t = Topic.get(new ObjectId(topic))
                 if (t == null) {
                     log.error("Topic does not exist")
-                    continue
+                    throw new InvalidUserInputException("Topic "+topic+" does not exist")
                 } else {
                     if (hasUserSelectedTopic(u, t)) {
                         continue
                     }
                 }
                 u.addToInterests(t)
+            }else{
+                throw new InvalidUserInputException("Invalid topic Id ");
             }
         }
     }
---
+++ grails-app/controllers/com/kllect/user/InvalidUserInputException.groovy
@@ -0,0 +1,10 @@
+package com.kllect.user
+
+/**
+ * Created by mingliangma on 2016-12-16.
+ */
+class InvalidUserInputException extends RuntimeException {
+    InvalidUserInputException(String s) {
+        super(s)
+    }
+}
